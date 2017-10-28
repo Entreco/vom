1233bc5aac7939db3613b9ca6dca9048423ddfea
Remco Janssen <Entreco@gmail.com>
2017-10-28 11:37:24 +0200

    Add simple site html for instant app

---
+++ website/index.php
@@ -0,0 +1,40 @@
+<!doctype html>
+<html amp lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>Vandaag of Morgen?</title>
+    <link rel="canonical" href="http://example.ampproject.org/article-metadata.html" />
+    <meta name="viewport" content="width=device-width,minimum-scale=1,initial-scale=1">
+    <script type="application/ld+json">
+      {
+        "@context": "http://schema.org",
+        "@type": "NewsArticle",
+        "headline": "Vandaag of Morgen",
+        "datePublished": "2015-10-07T12:02:41Z",
+        "image": [
+          "logo.jpg"
+        ]
+      }
+    </script>
+    <style amp-boilerplate>body{-webkit-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-moz-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-ms-animation:-amp-start 8s steps(1,end) 0s 1 normal both;animation:-amp-start 8s steps(1,end) 0s 1 normal both}@-webkit-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-moz-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-ms-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-o-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}</style>
+      <noscript><style amp-boilerplate>body{-webkit-animation:none;-moz-animation:none;-ms-animation:none;animation:none}</style></noscript>
+      <!-- Custom Style -->
+      <style amp-custom><?php readfile( getcwd() . "/styles.css"); ?></style>
+      
+      <!-- Fonts -->
+      <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700,400italic|Libre+Baskerville" rel="stylesheet" type="text/css">
+      
+      <!-- Check this one -->
+    <script async src="https://cdn.ampproject.org/v0.js"></script>
+  </head>
+  <body>
+      <div class="doc_header">
+      <amp-img src="images/background_header.jpg" width="1180" height="430" layout="responsive" class="doc_header_bg"></amp-img>
+          <div class="doc_header_inner">
+              <h1 class="doc_header_title">Geld besparen?</h1>
+<p class="doc_header_text">Check hier wanneer je het beste kunt tanken: Vandaag of morgen</p>
+<hr>
+          </div>
+      </div>
+  </body>
+</html>
